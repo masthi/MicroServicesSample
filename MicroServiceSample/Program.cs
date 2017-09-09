ed95a85cd865af197d5b9b0ad176ee1ae88067ec
masthi <masthan.rao@gmail.com>
2017-09-10 00:20:13 +0530

    Sample App for creating MicroServices

---
+++ MicroServiceSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicroService4Net;
+using System.Web.Http;
+using MicroService4Net.ServiceInternals;
+namespace MicroServiceSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var microservice = new MicroService();
+            microservice.Run(args);
+        }
+
+    }
+    public class ExampleController:ApiController
+    {
+        [Route("Example")]
+        public string GetExample()
+        {
+            return "Example";
+        }
+    }
+    public class MicroServiceInstaller : ProjectInstaller { }
+    public class MicroSerivceService : InternalService { }
+}
